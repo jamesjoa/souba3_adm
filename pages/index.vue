8f5e61c64abaee5c0c23f9b83445d61675b80867
dogcolley <jinji0326@naver.com>
2021-01-08 13:38:06 +0900

    vue-nuxt : clean test

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,6 @@
 <template>
   <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
       <v-card>
         <v-card-title class="headline">
           Welcome to the Vuetify + Nuxt.js template
@@ -77,17 +73,11 @@
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-//import  from 'vuex'
 
 export default {
   components: {
-    Logo,
-    VuetifyLogo
   },
   created:function(){
-    //console.log(this.$store.state)
     this.$store.dispatch('TESTING');
   }
 }
